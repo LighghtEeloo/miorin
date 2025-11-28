c11a6b2679ee6e4dabbe7a0ee4bce5eba98f5fe4
LitiaEeloo <litiaeeloo@gmail.com>
2025-11-28 01:19:02 -0500

    repo: switch to Leptos

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod app;
 
-use app::App;
-use dioxus::prelude::*;
-use dioxus_logger::tracing::Level;
+use app::*;
+use leptos::prelude::*;
 
 fn main() {
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
-    launch(App);
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! {
+            <App/>
+        }
+    })
 }
